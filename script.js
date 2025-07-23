82a30fffe90a52b5c744f148f4fd96100e3a3be0
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-23 19:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const values = document.querySelectorAll('.price')
+	let sum = 0;
+	values.forEach(item =>{
+		sum += parseInt(item.textContent)
+	});
+  const totalCell = document.createElement('td');
+  totalCell.textContent = sum;
+  totalCell.style.fontWeight = 'bold';
+	document.body.appendChild(totalCell);
   
 };
 
